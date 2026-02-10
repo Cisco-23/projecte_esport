e0c59d24c0481c59879a1941b82947029a46b588
Jesús <153534945+Cisco-23@users.noreply.github.com>
2026-02-10 16:09:56 +0100

    Add files via upload

---
+++ projecte_esport/src/test/java/com/company/projecte_esport/ProjecteEsportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.projecte_esport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteEsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
